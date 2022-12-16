136fdd7da278b581c1fc70a7cfd5bdc5c07fac7c
mkbmain <michael.bourke@live.co.uk>
2022-12-16 19:30:11 +0000

    First Commit

---
+++ DebuggerLib/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace DebuggerLib
+{
+    public static class DebugHelper
+    {
+        public static event Action<int, int, Var[]> BreakPointHit;
+        public static Dictionary<int, bool> BreakPoints = new Dictionary<int, bool>();
+        public static bool Lock = false;
+
+        public static void BreakPoint(int spanStart, int spanLength, params Var[] variables)
+        {
+            if (BreakPointHit is null || !BreakPoints.TryGetValue(spanStart, out var breakPoint) || !breakPoint) return;
+
+            Lock = true;
+            BreakPointHit.Invoke(spanStart, spanLength, variables);
+            while (Lock)
+            {
+                Thread.Sleep(505);
+            }
+        }
+    }
+
+    public struct Var
+    {
+        public string Name;
+        public object Value;
+
+        public Var(string name, object value)
+        {
+            Name = name;
+            Value = value;
+        }
+    }
+}
---
+++ Example/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Square root by the Newton's method.
+            var a = 5.0;
+            var x = a;
+            Run(a, x);
+        }
+
+
+        static void Run(double a, double x)
+        {
+            var list = new List<double>();
+            for (var i = 0; i < 100; i++)
+            {
+                var xi = (x + a / x) / 2;
+                list.Add(xi);
+                if (x == xi) break;
+                x = xi;
+            }
+
+            Console.WriteLine(x);
+            Console.WriteLine(list.Aggregate("",(a,b)=>$"{a}{b}"));
+        }
+    }
+}
